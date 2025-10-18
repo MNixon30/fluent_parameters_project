de4f6307943dadc0877605bda8d79243ee7d361d
williamvigneron222 <166066193+williamvigneron222@users.noreply.github.com>
2025-10-18 00:36:24 -0400

    checkpoint

--- app.py
+++ app.py
@@ -23,11 +23,22 @@ def selectFolderCallBack():
     filedialog.askdirectory()
 
 """
-Display the GUi window for setup
+Sidebar element for all pages
+"""
+def getSidebar(window):
+    sidebar = ttk.Frame(window, width=window.winfo_reqwidth(), height=window.winfo_reqheight())
+
+    SIDEBAR_WIDTH = 25
+    ttk.Label(window, text="Label test 1", width=SIDEBAR_WIDTH, anchor="center").grid(column = 0, row = 0, pady=5)
+    ttk.Button(window, text="Setup", width=SIDEBAR_WIDTH, command=showSetupWindow(window)).grid(column = 0, row = 1, pady=5)
+    return sidebar
+
+"""
+Display the "setup" GUI page
 """
 def showSetupWindow(window):
     assert isinstance(window, ThemedTk)
-    ttk.Button(window, text="Select Folder", command=selectFolderCallBack).pack()
+    # ttk.Button(window, text="Select Folder", command=selectFolderCallBack).pack()
     # messagebox.askyesno(title="title", message='msg')
     return
 
@@ -39,10 +50,38 @@ window.geometry("800x600")
 # window.minsize(400, 400) # TODO
 
 
+# BEGIN TEST
+# content = ttk.Frame(window, width=800, height=600)
+# frame = ttk.Frame(content,  borderwidth=5, relief="ridge", width=200, height=200)
+# namelbl = ttk.Label(content, text="Name")
+# name = ttk.Entry(content)
+
+# onevar = True
+# twovar = True
+# threevar = True
+# one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
+# two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
+# three = ttk.Checkbutton(content, text="Three", variable=threevar, onvalue=True)
+# ok = ttk.Button(content, text="Okay")
+# cancel = ttk.Button(content, text="Cancel")
+
+# content.grid(column=0, row=0)
+# frame.grid(column=0, row=0, columnspan=3, rowspan=2)
+# namelbl.grid(column=3, row=0, columnspan=2)
+# name.grid(column=3, row=1, columnspan=2)
+# one.grid(column=0, row=5)
+# two.grid(column=1, row=5)
+# three.grid(column=2, row=5)
+# ok.grid(column=3, row=5)
+# cancel.grid(column=4, row=5)
+
+#END
+
 
 # d_bug: test element
 # ttk.Button(window, text="Quit", command=window.destroy).grid(column = 0, row = 1  )
 
 
 # showSetupWindow(window)
+getSidebar(window)
 window.mainloop()
